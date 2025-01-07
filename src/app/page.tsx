406122a02a9265dccd371636ff9b2c6b9c53fb32
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-01-08 00:10:14 +0530

    Added fonts to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Playfair_Display, Work_Sans } from "next/font/google";
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full gap-5">
       <div>
-        <h1 className="text-5xl font-bold uppercase text-pink-red">
+        <h1
+          className={`text-5xl font-bold uppercase text-pink-red ${playfairDisplay.className}`}
+        >
           Lady Ledger
         </h1>
       </div>
       <div>
-        <p className="text-xl font-semibold text-pink-red">
+        <p className={`text-xl text-pink-red ${workSans.className}`}>
           Official NSV Ladies Club Ledger
         </p>
       </div>
       <div>
         <Link href="/dashboard">
-          <Button className="p-6 bg-pink-red hover:bg-white hover:text-pink-red hover:duration-200 hover:ease-in">
+          <Button
+            className={`p-6 bg-pink-red hover:bg-white hover:text-pink-red hover:duration-200 hover:ease-in ${workSans.className}`}
+          >
             Welcome Saroja Laxmi
           </Button>
         </Link>
